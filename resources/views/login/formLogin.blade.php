3ff218f2137bd7bc349f49ba5e1f28160d51b4e3
Gabriel Lopes Teixeira <Gabriel.Teixeira@sicoob.com.br>
2020-12-22 17:19:18 -0300

    Configurando o Login

--- resources/views/login/formLogin.blade.php
+++ resources/views/login/formLogin.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+@extends('template')
 
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="{{ asset('site/bootstrap.css') }}">
-
-    <link rel=" stylesheet" href="css/styleLogin.css">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title>Loja</title>
-</head>
-
-<body>
+@section('login')
 
     <div class="container border-bottom border-right rounded">
         <div class="row">
@@ -26,6 +14,7 @@
                     <h1>Lojas R&A Nacionais e Importados</h1>
                     <div class="form-group">
                         <h3>Usuário</h3>
+                        @csrf
                         <input class="form-control rounded" type="email" name="email" id="email"
                             placeholder="Informe seu usuário">
                         <h3>Senha</h3>
@@ -39,11 +28,4 @@
             </div>
         </div>
     </div>
-
-
-
-    <script src="{{ asset('site/jquery.js') }}"></script>
-    <script src="{{ asset('site/bootstrap.js') }}"></script>
-</body>
-
-</html>
+@show
---
+++ resources/views/template.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{ asset('site/bootstrap.css') }}">
+
+    <link rel=" stylesheet" href="css/styleLogin.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Loja</title>
+</head>
+
+<body>
+    <script src="{{ asset('site/jquery.js') }}"></script>
+    <script src="{{ asset('site/bootstrap.js') }}"></script>
+</body>
+
+</html>
